8902000ea1ccf3e6712f693c631135367f019134
vi255 <vitoriasr255@hotmail.com>
2018-09-04 10:35:41 -0300

    criacao de classes

---
+++ projetoTeste/projetoTeste/Models/Empresa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetoTeste.Models
+{
+    public class Empresa
+    {
+        [Key]
+
+        public int IdEmpresa { get; set; }
+
+        [Required(ErrorMessage = "Digite o Nome Fantasia da Empresa")]
+        [StringLength(100, ErrorMessage = "O Tamanho Máximo de caracteres é {1}", MinimumLength = 10)]
+        public string NomeFantasia { get; set; }
+
+        public string RazaoSocial { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string EndRua { get; set; }
+
+        public int EndNumero { get; set; }
+
+        public string EndBairro { get; set; }
+
+        public string EndCidade { get; set; }
+
+        public string EndEstado { get; set; }
+
+        public string EndPais { get; set; }
+
+
+    }
+}
---
+++ projetoTeste/projetoTeste/Models/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetoTeste.Models
+{
+    public class Funcionario
+    {
+        [Key]
+
+        public int IdFuncionario { get; set; }
+
+        [Required(ErrorMessage = "Digite o Nome do Funcionário")]
+        [StringLength(100, ErrorMessage = "O Tamanho Máximo de caracteres é {1}", MinimumLength = 10)]
+
+        public string Sobrenome { get; set; }
+
+        public string EndRua { get; set; }
+
+        public string EndNumero { get; set; }
+
+        public string EndBairro { get; set; }
+
+        public string EndCidade { get; set; }
+
+        public string EndEstado { get; set; }
+
+        public string EndPais { get; set; }
+        
+    }
+}
