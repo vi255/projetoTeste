c3edce84a17c51ef5eb12d55d1e12f9aac99ddef
vi255 <vitoriasr255@hotmail.com>
2018-09-05 15:51:51 -0300

    Modificação MinimumLength

--- projetoTeste/projetoTeste/Models/Empresa.cs
+++ projetoTeste/projetoTeste/Models/Empresa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,25 +14,34 @@ namespace projetoTeste.Models
         public int IdEmpresa { get; set; }
 
         [Required(ErrorMessage = "Digite o Nome Fantasia da Empresa")]
-        [StringLength(100, ErrorMessage = "O Tamanho Máximo de caracteres é {1}", MinimumLength = 10)]
+        [StringLength(100, ErrorMessage = "O Tamanho Máximo de caracteres é {1}", MinimumLength = 1)]
+
+        [DisplayName("Nome Fantasia")]
         public string NomeFantasia { get; set; }
 
+        [DisplayName("Razão Social")]
         public string RazaoSocial { get; set; }
 
         public string CNPJ { get; set; }
 
+        [DisplayName("Rua")]
         public string EndRua { get; set; }
 
+        [DisplayName("Número")]
         public int EndNumero { get; set; }
 
+        [DisplayName("Bairro")]
         public string EndBairro { get; set; }
 
+        [DisplayName("Cidade")]
         public string EndCidade { get; set; }
 
+        [DisplayName("Estado")]
         public string EndEstado { get; set; }
 
+        [DisplayName("País")]
         public string EndPais { get; set; }
 
-
+      
     }
 }
